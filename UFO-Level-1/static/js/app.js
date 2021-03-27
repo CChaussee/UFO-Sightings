8352126bddff9d60d19655bd3febed28fb300a96
Chelsea Chaussee <cfchaussee@gmail.com>
2021-03-27 13:03:46 -0700

    tyring to append rows to table

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -7,6 +7,13 @@ tableData.forEach(function(report){
 // build table
 function buildTable (tableData) {
     tableData.forEach(function(ufoReport) {
-        var row
+        var row = tableData.append("tr")
+        row.append("td").attr('scope', 'row').text(ufoReport.datetime);
+        row.append("td").text(ufoReport.city);
+        row.append("td").text(ufoReport.state);
+        row.append("td").text(ufoReport.country);
+        row.append("td").text(ufoReport.shape);
+        row.append("td").text(ufoReport.comments);
+    })
     })
 }
