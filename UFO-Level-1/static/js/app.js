2977b20eadec77d2f096e893b6e4f0a236d74be9
Chelsea Chaussee <cfchaussee@gmail.com>
2021-03-30 18:52:40 -0700

    tutor session

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -2,29 +2,35 @@
 const tableData = data;
 // testing connection
 tableData.forEach(function(report){ 
-    console.log(`${report.city}: ${report.state}`);
+    console.log(`${report.city}: ${report.state}`)});
 
 let tbody = d3.select("#ufo-table");    
 
 // build table
-function buildTable (tableData) {
-    tableData.forEach(function(ufoReport) {
-        let row = tableData.append("tr")
-        row.append("td").attr('scope', 'row').text(ufoReport.datetime);
-        row.append("td").text(ufoReport.city);
-        row.append("td").text(ufoReport.state);
-        row.append("td").text(ufoReport.country);
-        row.append("td").text(ufoReport.shape);
-        row.append("td").text(ufoReport.comments);
-    })
-    }
-})
+function buildTable(tableData) {
+    tableData.forEach( row => {
+        const row1 = tbody.append("tr") 
+        let stick = row1.append("td")
+        
+        Object.values(row).forEach( event => {
+            stick.text(event)
+        })
+        
+    
+})};
+function filtering (tableData) {tbody.html("")
+   let stuff = filteredData.filter(tbody => tbody.datetime === inputValue)
+buildTable(stuff)
+}
+
+let row = d3.select("tbody").selectAll("td");
+
 console.log(tableData)
 let submit = d3.select("#filter-btn");
-let row = d3.select("tbody").selectAll("td");
 let inputElement = d3.select("#datetime");
 let inputValue = inputElement.property("value");
 let filteredData = tableData;
-let filtered = filteredData.filter(tbody => tbody.datetime === inputValue);
 
-console.log(filteredData);
+let search = d3.select("filter-btn").on("click", filtering);
+
+buildTable(tableData);
