7982fed1f27b8ca3130f4226be4b1f7022cb1dca
Chelsea Chaussee <cfchaussee@gmail.com>
2021-04-01 09:59:37 -0700

    got table to display information correctly

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,17 +1,31 @@
 // from data.js
 let tableData = data;
-// testing connection
-tableData.forEach(function(report){ 
-    console.log(`${report.city}: ${report.state}`)});
 //reference table body and not table like before
 let tbody = d3.select("tbody");    
 // when search button is clicked
 let submit = d3.select("#filter-btn");
 submit.on("click", function() {
     let datetimeSearch = d3.select("#dateime");
-    let datetimeValue = d3.datetimeSearch.property("value");
+    let datetimeValue = datetimeSearch.property("value");
     if (datetimeValue != "") {
-        data = data.filter(entry.datetime === datetimeValue);
+        tableData = tableData.filter(entry.datetime === datetimeValue);
     }
 
 })
+console.log(data);
+
+//trying to make table appear
+renderTable();
+function renderTable(){
+    d3.select("#tbodyid").empty();
+    console.log(tableData);
+    tableData.forEach(function(ufoSightings) {
+        console.log(ufoSightings);
+        var row = tbody.append("tr");
+        Object.entries(ufoSightings).forEach(function([key,value]) {
+            console.log(key,value);
+            var cell = tbody.append("td");
+            cell.text(value);
+        });
+    });
+}
