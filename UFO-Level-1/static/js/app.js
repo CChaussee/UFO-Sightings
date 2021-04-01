e78c269194e67e0647a3e59e8045bd290870686e
Chelsea Chaussee <cfchaussee@gmail.com>
2021-03-31 21:56:14 -0700

    lets try mapping

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -8,7 +8,10 @@ let tbody = d3.select("tbody");
 // when search button is clicked
 let submit = d3.select("#filter-btn");
 submit.on("click", function() {
-    let datetimeSearch = d3.select("datime");
+    let datetimeSearch = d3.select("#dateime");
     let datetimeValue = d3.datetimeSearch.property("value");
+    if (datetimeValue != "") {
+        data = data.filter(entry.datetime === datetimeValue);
+    }
+
 })
-console.log(datetimeSearch);
